34870a83a2737ef086117171de4dea15b634aeac
jasmith1125 <jasmith1125@yahoo.com>
2014-09-19 15:10:50 -0400

    adding dummy files to link to Project 1

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Joyce Smith DWA Portfolio</title>
+    <link rel="stylesheet" href="css/foundation.css" />
+     <link href='http://fonts.googleapis.com/css?family=Kaushan+Script' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Kaushan+Script%7cOpen+Sans:400,300,600%7cOpen+Sans+Condensed:300,700' rel='stylesheet' type='text/css'>
+      <link rel="stylesheet" href="css/app.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+    <div class="row">
+      <div class="small-9 small-centered columns">
+        <h1 id="top">Dynamic Web Applications</h1>
+        <hr class="horizrule" />
+          <h2>PROJECT 2 GOES HERE</h2>
+        </div>
+      </div>
+      
+      </div>
+    
+    <script src="js/vendor/jquery.js"></script>
+    <script src="js/foundation.min.js"></script>
+    <script>
+      $(document).foundation();
+    </script>
+  </body>
+</html>
